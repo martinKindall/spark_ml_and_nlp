cfe7c79a08748f2d5b6d305d0017d1320ff7a78b
martinKindall <mart256@gmail.com>
2020-07-18 13:03:38 -0400

    New pipeline for context spell checking.

---
+++ ContextSpellCheckerExample.py
@@ -0,0 +1,58 @@
+from pyspark.sql import SparkSession
+from sparknlp.annotator import RecursiveTokenizer, ContextSpellCheckerModel
+from sparknlp.base import *
+from pyspark.ml import Pipeline
+
+
+def init_spark():
+    spark = SparkSession \
+        .builder \
+        .appName("HelloWorld") \
+        .config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp_2.11:2.5.3s") \
+        .config("spark.kryoserializer.buffer.max", "1000M") \
+        .getOrCreate()
+    sc = spark.sparkContext
+    return spark, sc
+
+
+def main():
+    spark, sc = init_spark()
+
+    tweetsDf = spark.read \
+        .load("C:\\sparkTmp\\why_i_wear_mask_tweets.csv",
+              format="csv", sep=",", inferSchema="true",
+              header="true", charset="UTF-8") \
+        .select("text")
+
+    pipeline = spellCheckerPipeline()
+    pipelineModel = pipeline.fit(tweetsDf)
+
+
+def spellCheckerPipeline():
+    documentAssembler = DocumentAssembler() \
+        .setInputCol("text") \
+        .setOutputCol("document")
+    tokenizer = RecursiveTokenizer() \
+        .setInputCols(["document"]) \
+        .setOutputCol("token") \
+        .setPrefixes(["\"", "(", "[", "\n"]) \
+        .setSuffixes([".", ", ", "?", ")", "!", "‘s"])
+    spellModel = ContextSpellCheckerModel \
+        .pretrained() \
+        .setInputCols("token") \
+        .setOutputCol("checked")
+    finisher = Finisher() \
+        .setInputCols("checked")
+
+    pipeline = Pipeline(stages=[
+        documentAssembler,
+        tokenizer,
+        spellModel,
+        finisher
+    ])
+
+    return pipeline
+
+
+if __name__ == '__main__':
+    main()
